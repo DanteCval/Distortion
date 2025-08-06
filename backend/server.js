7a1c27dc2b4002d4fd9c56bab3fc5b970243ea32
DanteCval <castaneda.dante18prog@gmail.com>
2025-08-06 15:26:27 -0600

    calando BD

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json()); // Suficiente para parsear JSON
 
-// SERVIR FRONTEND
+// SERVIR FRONTEND :)))
 app.use(express.static(path.join(__dirname, 'frontend')));
 
 app.get('/', (req, res) => {
